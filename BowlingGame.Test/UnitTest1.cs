61a1ce8c6bdeaeef89d15bca938f1f1db745d4db
Aurelien49 <92448486+AurelienV49@users.noreply.github.com>
2022-03-24 09:56:44 +0100

    Update UnitTest1.cs
    Ajout de 2 tests unitaires

--- BowlingGame.Test/UnitTest1.cs
+++ BowlingGame.Test/UnitTest1.cs
@@ -6,8 +6,27 @@ namespace BowlingGame.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestQuillesDown()
         {
+            // Test si toutes les quilles sont tombées
+
+            int nbquilleInit = 10;
+            int nbquillesDown = 10;
+
+            // Assert
+            Assert.AreEqual(nbquilleInit, nbquillesDown, 0.001, "Il reste des quilles à faire tomber");
+        }
+
+        [TestMethod]
+        public void TestQuillesDown2()
+        {
+            // Test si toutes les quilles sont tombées
+
+            int nbquilleInit = 10;
+            int nbquillesDown = 9;
+
+            // Assert
+            Assert.AreEqual(nbquilleInit, nbquillesDown, 0.001, "Il reste des quilles à faire tomber");
         }
     }
 }
